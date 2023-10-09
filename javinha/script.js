a70e2bf7c9f006d421012274aaab062c47c8b700
FabioSouzaOrcus09 <143653741+FabioSouzaOrcus09@users.noreply.github.com>
2023-10-09 17:50:42 -0300

    Add files via upload

---
+++ javinha/script.js
@@ -0,0 +1,47 @@
+"use strict";
+// alert ("alert") 
+let nome="Carlinhos";
+let pontos=45;
+let medida="47";
+let bloqueio=false;
+let vallor=true;
+const TAXA=3.45;
+
+console.log (typeof medida);
+medida=Number (medida);
+
+
+nome=23;
+alert(nome);
+alert(TAXA);
+medida=String (medida);
+console.log(medida);
+let valor=84758;
+console.log();
+
+
+let livros= [ 'senhor dos aneis', 'pweci jackson', 'harry pote'];
+console.log(livros [0]);
+
+/* o typeof mostra o tipo de arquivo, se é string ou number. È usado geralmente para quando você quer colocar como número somente, ou texto
+
+let teste= "digite um número"
+alert (typeof teste)
+let teste2= 2
+alert (typeof teste2)
+/*Trocando valor da variavel
+alert (nome);*/
+
+
+let carro= "monza";
+
+alert (`seu carro é um ${carro}, parabéns`);
+
+let valores= confirm ("escolha uma das opções");
+alert (`o valor escolhido foi ${valores}`);
+
+let idade=prompt("digite sua idade");
+console.log (`sua idade é ${idade}`);
+
+
+
